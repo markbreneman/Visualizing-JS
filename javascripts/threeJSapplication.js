ecf9380c905289d9941f64caa68db5152443aa93
mark breneman <mark.breneman@gmail.com>
2012-10-04 18:07:49 -0400

    commented out threeJS controls

--- javascripts/threeJSapplication.js
+++ javascripts/threeJSapplication.js
@@ -397,22 +397,22 @@ function setupThree(){
 
 
 
-function addControls(){
-
-	window.controls = new THREE.TrackballControls( camera )
-
-	controls.rotateSpeed = 1.0
-	controls.zoomSpeed   = 1.2
-	controls.panSpeed    = 0.8
-
-	controls.noZoom = false
-	controls.noPan  = false
-	controls.staticMoving = true
-	controls.dynamicDampingFactor = 0.3
-	controls.keys = [ 65, 83, 68 ]//  A, S, D
-
-	controls.addEventListener( 'change', render )
-}
+// function addControls(){
+// 
+// 	window.controls = new THREE.TrackballControls( camera )
+// 
+// 	controls.rotateSpeed = 1.0
+// 	controls.zoomSpeed   = 1.2
+// 	controls.panSpeed    = 0.8
+// 
+// 	controls.noZoom = false
+// 	controls.noPan  = false
+// 	controls.staticMoving = true
+// 	controls.dynamicDampingFactor = 0.3
+// 	controls.keys = [ 65, 83, 68 ]//  A, S, D
+// 
+// 	controls.addEventListener( 'change', render )
+// }
 
 
 
