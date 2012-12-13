10e53ff150ea2c5d9906eecce113bd036c8e9b72
markbreneman <mark.breneman@gmail.com>
2012-12-13 14:35:20 -0500

    got totalling of money for one city working, and nodes array in place...working on logic of links.

--- 112912 KickstarterScrape/KickStarterScrape.rb
+++ 112912 KickstarterScrape/KickStarterScrape.rb
@@ -44,13 +44,33 @@ get '/data' do
 	# projects = Kickstarter.by_citiesfunding(:San_Francisco,:page => 1, :pages=> 1)
 	# => returns back an array of projects from the cities/cityname/funding
 	projects = Kickstarter.by_citiesfunded(:New_York,:page => 1, :pages=> 1)
+	sfprojects = Kickstarter.by_citiesfunded(:San_Francisco,:page => 1, :pages=> 1)
 	# => returns back an array of projects from the cities/cityname/successful
 
 	 # puts JSON.parse(projects)
+	links = Array.new(15, Hash.new)
+	# links=[
+ #    {"source":1,"target":0,"value":1},
+ #    {"source":2,"target":0,"value":8},
+ #    {"source":3,"target":0,"value":10},
+ #    {"source":3,"target":2,"value":6},
+ #    {"source":4,"target":0,"value":1},
+ #    {"source":5,"target":0,"value":1},
+ #    {"source":6,"target":0,"value":1},
+ #    {"source":7,"target":0,"value":1},
+ #    {"source":8,"target":0,"value":2},
+ #    {"source":9,"target":0,"value":1},
+ #    {"source":11,"target":10,"value":1},
+ #    {"source":11,"target":3,"value":3},
+ #    {"source":11,"target":2,"value":3},
+ #    {"source":11,"target":0,"value":5},
+ #    {"source":12,"target":11,"value":1},
+ #  ]
 
+	projectsArray=Array.new
 
-	count=0
-	projectsJSON=Hash.new 
+	
+	projectsJSON=Hash.new  
 	projects.each do |project|
 	 projectobject=Hash.new 
 	 projectobject["name"]=project.name
@@ -62,13 +82,37 @@ get '/data' do
 	 projectobject["pledge_percent"]=project.pledge_percent
 	 projectobject["thumbnail_url"]=project.thumbnail_url
 	 projectobject["location"]=project.location
+	 projectobject["group"]="1"
 	 projectobject.to_json
-	 count+=1
-	 projectsJSON[count]=projectobject
+	 projectsArray.push projectobject
 	end
+	
+	total_pledged=0
+	total_pledgedpercent=0
+	projectsArray.each do |project|
+		total_pledged+=project["pledge_amount"]
+		total_pledgedpercent+=project["pledge_percent"]
+	end
+	# puts total_pledged
 
+	average_pledged=total_pledgedpercent/projectsArray.length
+   
+    newyorkobject=Hash.new
+ 	newyorkobject["name"]="New York"
+	 newyorkobject["url"]="http://www.kickstarter.com/discover/cities/new-york-ny/successful"
+	 newyorkobject["pledge_amount"]=total_pledged
+	 newyorkobject["pledge_percent"]=average_pledged
+	 newyorkobject["location"]="New_York"
+	 newyorkobject["group"]="1"
+	projectsArray.insert(0, newyorkobject)
+
+    projectsJSON["nodes"]=projectsArray
+	# projectsJSON["links"]=links
+	
   # @projects=projects	
   @projects=projectsJSON.to_json
+  @sfprojects=sfprojects
+
 
 
   erb :data
